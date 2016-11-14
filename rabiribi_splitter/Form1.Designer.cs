72361f3667d6d89f1da594697daf0e5daf6621ad
Copy Liu <copyliu@gmail.com>
2016-11-14 22:23:08 +0800

    prototype

---
+++ rabiribi_splitter/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace rabiribi_splitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbStatus = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mapLabel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.bossLabel = new System.Windows.Forms.Label();
+            this.portNum = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.portNum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "LiveSplit Server Port:";
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(212, 8);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(75, 23);
+            this.connectBtn.TabIndex = 4;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rabi-Ribi Status:";
+            // 
+            // rbStatus
+            // 
+            this.rbStatus.AutoSize = true;
+            this.rbStatus.Location = new System.Drawing.Point(126, 37);
+            this.rbStatus.Name = "rbStatus";
+            this.rbStatus.Size = new System.Drawing.Size(41, 12);
+            this.rbStatus.TabIndex = 6;
+            this.rbStatus.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Current Map:";
+            // 
+            // mapLabel
+            // 
+            this.mapLabel.AutoSize = true;
+            this.mapLabel.Location = new System.Drawing.Point(126, 58);
+            this.mapLabel.Name = "mapLabel";
+            this.mapLabel.Size = new System.Drawing.Size(41, 12);
+            this.mapLabel.TabIndex = 8;
+            this.mapLabel.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 78);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Current Boss:";
+            // 
+            // bossLabel
+            // 
+            this.bossLabel.AutoSize = true;
+            this.bossLabel.Location = new System.Drawing.Point(126, 80);
+            this.bossLabel.Name = "bossLabel";
+            this.bossLabel.Size = new System.Drawing.Size(41, 12);
+            this.bossLabel.TabIndex = 10;
+            this.bossLabel.Text = "label8";
+            // 
+            // portNum
+            // 
+            this.portNum.Location = new System.Drawing.Point(149, 9);
+            this.portNum.Maximum = new decimal(new int[] {
+            65536,
+            0,
+            0,
+            0});
+            this.portNum.Name = "portNum";
+            this.portNum.Size = new System.Drawing.Size(57, 21);
+            this.portNum.TabIndex = 11;
+            this.portNum.Value = new decimal(new int[] {
+            16834,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 221);
+            this.Controls.Add(this.portNum);
+            this.Controls.Add(this.bossLabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.mapLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rbStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.connectBtn);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Irisu is watching you";
+            ((System.ComponentModel.ISupportInitialize)(this.portNum)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label rbStatus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label mapLabel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label bossLabel;
+        private System.Windows.Forms.NumericUpDown portNum;
+    }
+}
+
---
+++ rabiribi_splitter/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace rabiribi_splitter
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess,
+            int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        const int PROCESS_WM_READ = 0x0010;
+
+        private static TcpClient tcpclient;
+        private static NetworkStream networkStream;
+        private static System.Threading.Timer timer;
+        private int MapAddress = 0xA3353C;
+        private int PtrAddr = 0x00940EE0;
+        private int EnitiyOffset = 0x4e4;
+        private int EntitySize = 0x6F4;
+        private int MaxEntityEntry = 50;
+        private bool bossbattle = false;
+
+        private static Dictionary<int, string> BossNames = new Dictionary<int, string>()
+        {
+            {1009, "Cocoa"},
+            {1011, "Rumi"},
+            {1012, "Ashuri"},
+            {1013, "Rita"},
+            {1014, "Ribbon"},
+            {1015, "Cocoa"},
+            {1018, "Cicini"},
+            {1020, "Saya"},
+            {1021, "Syaro"},
+            {1022, "Pandora"},
+            {1023, "Nieve"},
+            {1024, "Nixie"},
+            {1025, "Aruraune"},
+            {1030, "Seana"},
+            {1031, "Lilith"},
+            {1032, "Vanilla"},
+            {1033, "Chocolate"},
+            {1035, "Illusion Alius"},
+            {1036, "Pink Kotri"},
+            {1037, "Noah 1"},
+            {1038, "Irisu"},
+            {1039, "Miriam"},
+            {1043, "Miru"},
+            {1053, "Noah 3"},
+            {1054, "Keke Bunny"},
+
+
+        };
+
+        static string[] MapNames = new string[]
+        {
+            "Southern Woodland",
+            "Western Coast",
+            "Island Core",
+            "Northern Tundra",
+            "Eastern Highlands",
+            "Rabi Rabi Town",
+            "Plurkwood",
+            "Subterranean Area",
+            "Warp Destination",
+            "System Interior",
+        };
+
+        static int[][] MapBoss = new int[][]
+        {
+            new[] {1011, 1009, 1025, 1014},
+            new[] {1036, 1038, 1031, 1022, 1012},
+            new[] {1032, 1036, 1030, 1033},
+            new[] {1024, 1023, 1013, 1030},
+            new[] {1012, 1020,},
+            new int[0],
+            new[] {1054},
+            new[] {1036, 1039},
+            new[] {1037, 1053, 1035, 1043},
+            new[] {1021},
+
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer = new System.Threading.Timer(readmemory, null, 0, 10);
+
+
+        }
+
+        private void readmemory(object state)
+        {
+            var processlist = Process.GetProcessesByName("rabiribi");
+            if (processlist.Length > 0)
+            {
+                rbStatus.Text = "Running";
+                Process process = processlist[0];
+                byte[] buffer = new byte[4] {0, 0, 0, 0};
+                int bytesRead = 0;
+                IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+                ReadProcessMemory((int) processHandle, process.MainModule.BaseAddress.ToInt32() + MapAddress, buffer,
+                    1, ref bytesRead);
+                int mapid;
+                if (buffer[0] < MapNames.Length)
+                {
+                    this.mapLabel.Text = MapNames[buffer[0]];
+                    mapid = buffer[0];
+                    ReadProcessMemory((int) processHandle, process.MainModule.BaseAddress.ToInt32() + PtrAddr, buffer, 4,
+                        ref bytesRead);
+                    var ptr = BitConverter.ToInt32(buffer, 0) + EnitiyOffset;
+                    List<int> bosses = new List<int>();
+                    for (var i = 0; i < 50; i++)
+                    {
+                        ptr += 0x6f4;
+                        ReadProcessMemory((int) processHandle, ptr, buffer, buffer.Length, ref bytesRead);
+                        var emyid = BitConverter.ToInt32(buffer, 0);
+                        if (BossNames.ContainsKey(emyid))
+                        {
+                            bosses.Add(emyid);
+                        }
+
+                    }
+                    //Now checking map
+                    bool flag = false;
+                    this.bossLabel.Text = "";
+                    foreach (var i in MapBoss[mapid])
+                    {
+                        if (bosses.Contains(i))
+                        {
+                            flag = true;
+                            this.bossLabel.Text += BossNames[i] + " ";
+                        }
+                    }
+                    if (flag != bossbattle)
+                    {
+                        sendsplit();
+                    }
+                    bossbattle = flag;
+                }
+                else
+                {
+                    this.mapLabel.Text = "N/A";
+                    this.bossLabel.Text = "";
+                }
+
+
+            }
+            else
+            {
+                rbStatus.Text = "Not Found";
+                this.mapLabel.Text = "N/A";
+                this.bossLabel.Text = "";
+            }
+        }
+
+        void sendsplit()
+        {
+            if (tcpclient != null && tcpclient.Connected)
+            {
+                try
+                {
+                    var b = Encoding.UTF8.GetBytes("split\r\n");
+                    networkStream.Write(b,0,b.Length);
+                }
+                catch (Exception)
+                {
+
+                    disconnect();
+                }
+            }   
+        }
+
+        void disconnect()
+        {
+            tcpclient = null;
+            connectBtn.Enabled = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tcpclient != null && tcpclient.Connected) return;
+            try
+            {
+                tcpclient = new TcpClient("127.0.0.1", Convert.ToInt32(portNum.Value));
+                networkStream = tcpclient.GetStream();
+                connectBtn.Enabled = false;
+            }
+            catch (Exception)
+            {
+                tcpclient = null;
+                networkStream = null;
+                MessageBox.Show(this, "Connect Failed");
+
+            }
+        }
+    }
+}
