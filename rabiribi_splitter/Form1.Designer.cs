66cd5ed90f9d905da7e474161bfd52fc22479da8
Copy Liu <copyliu@gmail.com>
2016-11-17 22:22:16 +0800

    bump version

--- rabiribi_splitter/Form1.Designer.cs
+++ rabiribi_splitter/Form1.Designer.cs
@@ -220,7 +220,7 @@
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 24;
-            this.label4.Text = "v0.0.3";
+            this.label4.Text = "v0.0.4";
             // 
             // cbSideCh
             // 
