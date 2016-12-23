ef23c31744a78073efe13b2ed0e843e619feaf59
Copy Liu <copyliu@gmail.com>
2016-12-23 21:38:07 +0800

    update for 1.75

--- rabiribi_splitter/StaticData.cs
+++ rabiribi_splitter/StaticData.cs
@@ -149,22 +149,23 @@ namespace rabiribi_splitter
         };
 
 
-        public static int[] MapAddress =  { 0xA3353C, 0xA57020, 0xA5E0AC };
-        public static int[] EnenyPtrAddr = { 0x00940EE0, 0x00964A1C, 0x0096BA3C };
-        public static int[] EnenyEnitiyHPOffset = { 0x4c8, 0x4d0,0x4d8 };
-        public static int[] EnenyEnitiyIDOffset = { 0x4e4,0x4ec, 0x4F4 };
-        public static int[] EnenyEntitySize = {0x6F4, 0x6FC, 0x704 };
-        public static int[] MaxEntityEntry = { 50,50,50 };
-        public static int[] MoneyAddress = { 0xD3823C, 0xD5B9FC, 0xD63D2C };
-        public static string[] VerNames = {"1.65", "1.70","1.71"};
+        public static int[] MapAddress =  { 0xA3353C, 0xA57020, 0xA5E0AC, 0xD642C4 };
+        public static int[] EnenyPtrAddr = { 0x00940EE0, 0x00964A1C, 0x0096BA3C, 0x0096DA3C };
+        public static int[] EnenyEnitiyHPOffset = { 0x4c8, 0x4d0,0x4d8, 0x4d8 };
+        public static int[] EnenyEnitiyIDOffset = { 0x4e4,0x4ec, 0x4F4, 0x4F4 };
+        public static int[] EnenyEntitySize = {0x6F4, 0x6FC, 0x704, 0x704 };
+        public static int[] MaxEntityEntry = { 50,50,50 ,50};
+        public static int[] MoneyAddress = { 0xD3823C, 0xD5B9FC, 0xD63D2C, 0xD654CC };
+        public static string[] VerNames = {"1.65", "1.70","1.71","1.75"};
         public static int[] MusicAddr = 
         {
             0xA46294,
            0xA69D98,
-           0xA70E28
+           0xA70E28,
+           0x4FAB60
         };
 
-        public static int[] TownMemberAddr = {0xD38934, 0xD5C0F4, 0xD63BC4 };
+        public static int[] TownMemberAddr = {0xD38934, 0xD5C0F4, 0xD63BC4, 0xD65BC4 };
 
     }
 }
